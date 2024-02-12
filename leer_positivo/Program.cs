191484377b79e5efb954698468e3b95e6178dfbd
Michael112007 <117554393+Michael112007@users.noreply.github.com>
2024-02-12 12:48:23 -0400

    Agregar archivos de proyecto.

---
+++ leer_positivo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    Console.WriteLine("Determinando si es positivo o negativo");
+Console.WriteLine();
+
+    int numero1;
+
+    Console.WriteLine("numero1");
+    numero1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+    if (numero1 >= 0)
+    {
+        Console.WriteLine("el numero es positivo");
+    }
+
+    else
+    {
+        Console.WriteLine("el numero es negativo");
+    }
+
+}
